aa7ddda0af55bfc397e28f5a10234269b49ef76f
= <=>
2026-03-01 16:43:52 +0000

    Exercise 9 done solving DIP problem but the output doesn't match

--- SOLID/Ex9/src/EvaluationPipeline.java
+++ SOLID/Ex9/src/EvaluationPipeline.java
@@ -1,18 +1,24 @@
 public class EvaluationPipeline {
     // DIP violation: high-level module constructs concretes directly
-    public void evaluate(Submission sub) {
-        Rubric rubric = new Rubric();
-        PlagiarismChecker pc = new PlagiarismChecker();
-        CodeGrader grader = new CodeGrader();
-        ReportWriter writer = new ReportWriter();
+    private final PlagiarismChecker plagiarismChecker;
+    private final CodeGrader gradingService;
+    private final ReportWriter reportService;
 
-        int plag = pc.check(sub);
+        public EvaluationPipeline(PlagiarismChecker pc, CodeGrader cg, ReportWriter rw) {
+            this.plagiarismChecker = pc;
+            this.gradingService = cg;
+            this.reportService = rw;
+        }
+
+    public void evaluate(Submission sub) {
+
+        int plag = plagiarismChecker.check(sub);
         System.out.println("PlagiarismScore=" + plag);
 
-        int code = grader.grade(sub, rubric);
+        int code = gradingService.grade(sub);
         System.out.println("CodeScore=" + code);
 
-        String reportName = writer.write(sub, plag, code);
+        String reportName = reportService.write(sub, plag, code);
         System.out.println("Report written: " + reportName);
 
         int total = plag + code;
--- SOLID/Ex9/src/Main.java
+++ SOLID/Ex9/src/Main.java
@@ -1,7 +1,16 @@
 public class Main {
     public static void main(String[] args) {
         System.out.println("=== Evaluation Pipeline ===");
+        
         Submission sub = new Submission("23BCS1007", "public class A{}", "A.java");
-        new EvaluationPipeline().evaluate(sub);
+        
+        Rubric rubric = new Rubric();
+
+        PlagiarismChecker plag = new PlagiarismChecker();
+        CodeGrader grader = new CodeGrader(rubric);
+        ReportWriter writer = new ReportWriter();
+
+        EvaluationPipeline pipeline = new EvaluationPipeline(plag, grader, writer);
+        pipeline.evaluate(sub);
     }
 }
