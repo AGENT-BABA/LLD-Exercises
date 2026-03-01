364dd9599d1b478c6eeee1f807cc6084dc7b0fd0
= <=>
2026-03-01 15:33:32 +0000

    Exercise 6 done

--- SOLID/Ex6/src/SmsSender.java
+++ SOLID/Ex6/src/SmsSender.java
@@ -1,8 +1,15 @@
 public class SmsSender extends NotificationSender {
     public SmsSender(AuditLog audit) { super(audit); }
 
+    @Override 
+    protected void validate(Notification n) {
+        if (n.phone == null || n.phone.isEmpty()) {
+            throw new IllegalArgumentException("phone required");
+        }
+    }   
+
     @Override
-    public void send(Notification n) {
+    protected void doSend(Notification n) {
         // Ignores subject; base type doesn't clarify expectations (smell)
         System.out.println("SMS -> to=" + n.phone + " body=" + n.body);
         audit.add("sms sent");
