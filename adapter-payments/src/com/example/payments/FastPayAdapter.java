d4abc39ecba6ecee9aa6f665140f5e7e25190a8e
Krushna Sonawane <skrushna1983@gmail.com>
2026-03-09 07:04:51 +0530

    Adapter Exercise Done

---
+++ adapter-payments/src/com/example/payments/FastPayAdapter.java
@@ -0,0 +1,21 @@
+package com.example.payments;
+
+public class FastPayAdapter implements PaymentGateway{
+    private final FastPayClient client;
+
+    public FastPayAdapter(FastPayClient client) {
+        if(client == null) {
+            throw new IllegalArgumentException("client cannot be null");
+        }
+        this.client = client;
+    }
+
+    @Override
+    public String charge(String customerId, int amountCents) {
+        if(customerId == null || customerId.isEmpty()) {
+            throw new IllegalArgumentException("customerId cannot be null or empty");
+        }
+        return client.payNow(customerId, amountCents);
+    }
+    
+}
